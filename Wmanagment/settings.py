a25d89ef3fcdfde6df613d8007339160a7f7b9ba
malexit240 <alektoiii@gmail.com>
2020-01-05 15:28:36 +0200

    added pagination settings

--- Wmanagment/settings.py
+++ Wmanagment/settings.py
@@ -160,6 +160,9 @@ DEBUG_TOOLBAR_PANELS = [
 ]
 
 REST_FRAMEWORK = {
+    'PAGE_SIZE': 10,
+    'DEFAULT_PAGINATION_CLASS':
+    'rest_framework.pagination.PageNumberPagination',
     'DEFAULT_AUTHENTICATION_CLASSES': [
         'rest_framework.authentication.TokenAuthentication',
     ],
